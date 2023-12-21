43883e6bf174c11f132b9f4607bf2820f0af4ce5
Matteo Prendi <mateoprendi@proton.me>
2023-12-21 09:08:34 +0100

    Testing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { View, Text } from "react-native";
 
-export const applyCustomCode = (externalCodeSetup) => {
-  externalCodeSetup.navigationApi.replaceScreenComponent("LoginScreen", () => (
+const applyCustomCode = (externalCodeSetup) => {
+  externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Hello from custom Login Screen</Text>
     </View>
