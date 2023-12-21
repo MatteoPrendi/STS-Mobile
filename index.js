3d1a55f5241595a524e06f0fbda1bc1083a67c9f
Matteo Prendi <mateoprendi@proton.me>
2023-12-21 09:10:47 +0100

    Debugging

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 
 export const applyCustomCode = (externalCodeSetup) => {
-  return (
-    <View>
-      <Text>Hi</Text>
+  externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Testing...</Text>
     </View>
-  );
+  ));
 };
